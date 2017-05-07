a677fea28169932896b5f460ff05a02bd56c23f5
Matt Pilcher <mattpilcher87@gmail.com>
2017-05-07 22:49:04 +0100

    Timeline laurel, footer link animation, mobile amends, timeline background image

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,3 +1,4 @@
+//- Navigation
 var navOpen = false;
 
 function toggleNavigation() {
@@ -46,7 +47,6 @@ function toggleNavigation() {
 })();
 
 
-
 //- Events
 function changeEvent(event) {
     $(".events-showcase--container").removeClass("is-active");
@@ -60,20 +60,18 @@ function changeEvent(event) {
 
 
 //- Wow
-
 var wow = new WOW(
   {
-    boxClass:     'wow',      // animated element css class (default is wow)
-    animateClass: 'animated', // animation css class (default is animated)
-    offset:       200,          // distance to the element when triggering the animation (default is 0)
-    mobile:       false,       // trigger animations on mobile devices (default is true)
-    live:         true,       // act on asynchronously loaded content (default is true)
+    boxClass:     'wow',
+    animateClass: 'animated',
+    offset:       200,
+    mobile:       false,
+    live:         true,
     callback:     function(box) {
-      // the callback is fired every time an animation is started
-      // the argument that is passed in is the DOM node being animated
+
     },
-    scrollContainer: null,    // optional scroll container selector, otherwise use window,
-    resetAnimation: true,     // reset animation on end (default is true)
+    scrollContainer: null,
+    resetAnimation: true,
   }
 );
 wow.init();
